b028a26561ba9702b2845ec9f12f7adfdb26cb0b
Pankaj Vaghela <vpankaj1998official@gmail.com>
2020-04-16 03:22:28 +0530

    Api for creating acl data , with test case

--- tests/runner.js
+++ tests/runner.js
@@ -3,22 +3,35 @@ var SequelizeAcl = require('../lib/index')
     , tests = require('./tests')
 
 
+
 describe('Sequelize ACL - SQLite', function () {
 
     var dbConfig = {
         dialect: 'sqlite',
         logging: false,
-        // storage: './seql-acl-testdb5.sqlite3',
+        // storage: './seql-acl-testdb1.sqlite3',
     }
+
+    it('should make acl object', function(done){
+        let self = this;
+
+        let seq = new Sequelize({ dialect: 'sqlite',logging: false, });        
+        seq.authenticate().then(function() {
+            self.acl2 =  new SequelizeAcl(seq, {sync : false, debug: true, timestamps : true, paranoid : true});
+            done();
+        });
+    });
 
     before(function (done) {
         let self = this;
         let seq = new Sequelize(dbConfig);
-
+        
         seq.authenticate().then(function() {
             return seq.drop().then(function() {
                 self.acl = new SequelizeAcl(seq, {});
+    
                 done();
+                
             });
         }).catch(done);
     })
--- tests/tests.js
+++ tests/tests.js
@@ -11,19 +11,52 @@ exports.Constructor = function () {
 
     // Group of tests using describe
     describe('setup', function () {
-        it('properly runs up migration', function() {
-            var migration = [];
-            var queryInterfaceStub = {
-                createTable: function(tableName, schema) {
-                    migration.push(tableName);
-                }
-            };
 
+        let migration = [];
+        let queryInterfaceStub = {
+            createTable: function(tableName, schema) {
+                migration.push(tableName);
+            },
+            dropTable: function(tableName, schema) {
+                migration.push(tableName);
+            }
+        };
+
+        it('properly runs up migration', function() {
+
+            migration = [];
             // ['acl_actions', 'acl_resources', 'acl_permissions', 'acl_roles', 'acl_role_user', 'acl_role_permission'  ];
             let expected = SequelizeAcl.migration.tables.map(t => 'acl_'+t);
 
-            SequelizeAcl.migration.up(queryInterfaceStub, this.acl.sequelize, { prefix: 'acl_' });
+            SequelizeAcl.migration.up(queryInterfaceStub, this.acl.sequelize);
 
+            assert.deepEqual(migration, expected);
+        });
+
+        it('properly runs up migration with options', function() {
+            migration = [];
+            // ['acl_actions', 'acl_resources', 'acl_permissions', 'acl_roles', 'acl_role_user', 'acl_role_permission'  ];
+            let expected = SequelizeAcl.migration.tables.map(t => 'acl_'+t);
+
+            SequelizeAcl.migration.up(queryInterfaceStub, this.acl.sequelize, { prefix: 'acl_', timestamps: true , paranoid : true});
+
+            assert.deepEqual(migration, expected);
+        });
+        
+        it('properly runs down migrations', function() {
+            migration = [];            
+            // ['acl_actions', 'acl_resources', 'acl_permissions', 'acl_roles', 'acl_role_user', 'acl_role_permission'  ];
+            let expected = SequelizeAcl.migration.tables.map(t => 'acl_'+t);
+
+            SequelizeAcl.migration.down(queryInterfaceStub, this.acl.sequelize);
+            assert.deepEqual(migration, expected);
+        });
+        it('properly runs down migrations with options', function() {
+            migration = [];            
+            // ['acl_actions', 'acl_resources', 'acl_permissions', 'acl_roles', 'acl_role_user', 'acl_role_permission'  ];
+            let expected = SequelizeAcl.migration.tables.map(t => 'acl_'+t);
+
+            SequelizeAcl.migration.down(queryInterfaceStub, this.acl.sequelize, { prefix: 'acl_' });
             assert.deepEqual(migration, expected);
         });
     });
@@ -34,36 +67,62 @@ exports.Permissions = function(){
     describe('Permissions', function(){
         it('should create permission single action', function(done){
 
-            this.acl.makePermission('blogs','view').then(function(data){
-                expect(data.dataValues.name).to.equal('blogs:[view]');
-                done();
-            }).catch((err)=>{
-                console.log(err);
-
-                assert(!err);
-                done();
-            })
+            this.acl.createPermissions('blog','view')
+                .then(function(perms){
+                    expect(perms[0].name).to.equal('blog:[view]');
+                    done();
+                }).catch((err)=>{
+                    assert(!err);
+                    done();
+                })
         })
         it('should create permission multiple actions', function(done){
-            this.acl.makePermission('blogs',['view','edit']).then(function(data){
-                expect(data.dataValues.name).to.equal('blogs:[view,edit]');
-                done();
-            }).catch((err)=>{
-                console.log(err);
-
-                assert(!err);
-                done();
-            })
+            this.acl.createPermissions('blog',['view','edit'])
+                .then(function(perms){
+                    expect(perms[0].name).to.equal('blog:[view,edit]');
+                    done();
+                }).catch((err)=>{
+                    assert(!err);
+                    done();
+                })
         })
         it('should not create duplicate permission', function(done){
-            this.acl.makePermission('blogs',['view','edit']).then(function(data){
-                expect(!data.dataValues)
-                done();
-            }).catch((err)=>{
-                assert(err);
-                done();
-            })
+            this.acl.createPermissions('blog',['view','edit'])
+                .then(function(perms){
+                    expect(perms.length).to.equal(0);
+                    done();
+                }).catch((err)=>{
+                    assert(!err);
+                    done();
+                })
+        });
+
+        it('should create permission with "multiple resources" return created', function(done){
+            this.acl.createPermissions(['blog','users'],['view','edit'], { 
+                    names:['','blog_main'],
+                    json :true
+                }).then(function(perms){
+                    expect(perms.length).to.equal(1);
+                    done();
+                }).catch((err)=>{
+                    assert(!err);
+                    done();
+                })
+        });
+
+        it('should create permission with "multiple resources" return all', function(done){
+            this.acl.createPermissions(['blog','users','admin'],['view','edit'], { 
+                    names:['','blog_main'],
+                    all: true
+                }).then(function(perms){
+                    expect(perms.length).to.equal(3);
+                    done();
+                }).catch((err)=>{
+                    assert(!err);
+                    done();
+                })
         })
+
     });
 }
 
@@ -71,9 +130,18 @@ exports.Roles = function(){
     describe('Roles', function(){
 
         describe('Create Role(s)', function(){
-            it('single: should create role', function(done){
+            it('single: should create role by string', function(done){
                 this.acl.makeRole('admin').then(function(data){
                     expect(data.role.dataValues.name).to.equal('admin');
+                    done();
+                }).catch((err)=>{
+                    assert(!err);
+                    done();
+                })
+            });
+            it('single: should create role by object', function(done){
+                this.acl.makeRole({name : 'someadmin'}).then(function(data){
+                    expect(data.role.dataValues.name).to.equal('someadmin');
                     done();
                 }).catch((err)=>{
                     assert(!err);
@@ -92,7 +160,7 @@ exports.Roles = function(){
             })
 
             it('multiple: should not create duplicate, return all roles', function(done){
-                this.acl.makeRoles(['admin','analyst',{ name:'user', description:"A basic user"}], {all : true}).then(function(data){
+                this.acl.makeRoles(['admin','analyst',{ name:'user', description:"A basic user"}], {all : true, json:true}).then(function(data){
                     expect(data.length).to.equal(3);
                     done();
                 }).catch((err)=>{
@@ -114,7 +182,7 @@ exports.Roles = function(){
             });
 
             it('multiple: should delete roles', function(done){
-                this.acl.deleteRoles(['admin', 'moderator']).then(function(data){
+                this.acl.deleteRoles(['admin', {name :'moderator'}]).then(function(data){
                     expect(data).to.equal(1);
                     done();
                 }).catch((err)=>{
@@ -124,6 +192,15 @@ exports.Roles = function(){
             });
         })
 
+        it('should throw error for array as argument for role', function(done){
+            this.acl.deleteRoles([3,'analyst', {name :'moderator'}]).then(function(data){
+                assert(!data);
+                done();
+            }).catch((err)=>{
+                assert(err);
+                done();
+            })
+        });
 
     });
 }
@@ -186,24 +263,21 @@ exports.UserAclified = function(){
                             expect(roles.length).to.equal(2);
                             done();
                         })
-                    })
+                    })  
                 });
             })
 
-            // it('multiple : should remove roles', function(done){
-            //     this.acl._sequelize.models.User.findByPk(1).then(function(user){
-            //         user.assignRoles(['admin', 'moderator']).then(result =>{
-            //             user.getRoles().then(roles => {
-            //                 expect(roles.length).to.equal(2);
-            //                 done();
-            //             })
-            //         })
-            //     }).catch((err)=>{
-            //         console.log(err);
-            //         assert(!err);
-            //         done();
-            //     })
-            // });
+            it('multiple : should remove roles', function(done){
+                this.acl._sequelize.models.User.findByPk(1).then(function(user){
+                    user.rmRoles(['superadmin','moderator','toddle']).then(result => {
+                        expect(result).to.equal(2);
+                        user.getRoles().then(roles => {
+                            expect(roles.length).to.equal(0);
+                            done();
+                        })
+                    })
+                });
+            });
 
         });
     });
@@ -212,37 +286,76 @@ exports.UserAclified = function(){
 exports.MakeControl = function(){
     describe('MakeControl', function(){
         it('add admin role to control', function(){
-            let res = this.acl.make().allow('admin');
+            let res = this.acl.init().allow('admin');
 
-            expect(res._roles).to.include('admin');
+            expect(res._control._roles).to.include('admin');
         });
+        // it('add multiple admin role to control', function(){
+        //     let res = this.acl.init().allow(['admin','moderator']);
+        //     expect(res._control._roles.length).to.equal(2);
+        // });
 
         it('add view action to control', function(){
             let res = this.acl.to('view');
-
-            expect(res._actions).to.include('view');
+            expect(res._control._actions).to.include('view');
         });
 
         it('add blog resource to control', function(){
             let res = this.acl.on('blog');
-
-            expect(res._resources).to.include('blog');
+            expect(res._control._resources).to.include('blog');
+        });
+        it('should reset control sets', function(){
+            let res = this.acl.init();
+            expect(res._control._roles.length).to.equal(0);
+            expect(res._control._actions.length).to.equal(0);
+            expect(res._control._resources.length).to.equal(0);
         });
 
-        // it('commit control', async function(){
-        //     await this.acl.allow(['public','mag']).commit(function(data, err){
-        //         assert(!err);
-        //         // done();
-        //     });
-        //     // done();
-        // });
+        it('commit basic control', function(done){
+            this.acl.init()
+                .allow('admin')
+                .to(['view', 'edit'])
+                .on('blog').commit().then((data) => {
+                    done();
+                }).catch(e => {
+                    assert(!e)
+                    done();
+                });
+        });
 
-        // it('dont allow duplicate role', function(done){
-        //     this.acl.make().allow(['public']).commit(function(data, err){
-        //         assert(err);
-        //         done();
-        //     });
+        it('commit basic control 2', function(done){
+            this.acl.init()
+                .allow('superadmin')
+                .to(['*'])
+                .on('blog').commit().then((data) => {
+                    done();
+                }).catch(e => {
+                    assert(!e);
+                    done();
+                });
+        });
+        it('commit basic control 3', function(done){
+            this.acl.init()
+                .allow('superadmin')
+                .to(['*'])
+                .on(['blog','post','image']).commit().then((data) => {
+                    done();
+                }).catch(e => {
+                    assert(!e);
+                    done();
+                });
+        });
+        
 
-        // });
+    })
+}
+
+exports.Miscl = function(){
+
+    describe('Miscl', function(){
+
+        if(this.acl2){
+            
+        }
     })
 }
