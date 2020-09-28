cc926d9b0cc700255f1b5331284211fc383c3f82
nathanlepori <nathan.lepori@hotmail.com>
2020-09-28 14:26:35 +0200

    Added type definitions

--- index.d.ts
+++ index.d.ts
@@ -1,29 +1,167 @@
-import {Model, Sequelize} from 'sequelize';
+import * as sequelize from 'sequelize/types/lib/sequelize';
+import { Model, ModelType, Sequelize } from 'sequelize';
+import EventEmitter = NodeJS.EventEmitter;
+import NodeCache from 'node-cache';
 
-interface SequelizeGuardOptions {
+declare module 'sequelize/types/lib/sequelize' {
+  interface Sequelize {
+    guard: SequelizeGuard;
+  }
+}
+
+type Action = 'view' | 'create' | 'update' | 'delete' | '*' | 'approve';
+
+interface SequelizeGuardOptions<TModel extends ModelType = typeof User> {
   prefix?: string;
   primaryKey?: string;
   timestamps?: boolean;
   paranoid?: boolean;
   sync?: boolean;
   debug?: boolean;
-  UserModel?: Model;
+  UserModel?: TModel;
   userPk?: string;
   safeGuardDeletes?: boolean;
   userCache?: boolean;
   userCacheTtl?: number;
 }
 
-interface UserModel {
-  assignRole(role: string): UserModel;
+interface GuardUser {
+  assignRole(role: string): Promise<this>;
+  assignRoles(roles: string[]): Promise<RoleUser>;
+  rmAssignedRoles(roles: string[]): Promise<void>;
+  can(permission: string): Promise<boolean>;
+  cant(permission: string): Promise<boolean>;
+  isAllOf(roles: string[]): Promise<boolean>;
+  isAnyOf(roles: string[]): Promise<boolean>;
+  isA(role: string): Promise<boolean>;
+  isAn(role: string): Promise<boolean>;
+  roles(): Promise<GuardRole>;
 }
 
-declare class SequelizeGuard {
-  constructor(seql: Sequelize, options: SequelizeGuardOptions);
+/* ---------- Models ---------- */
+declare class User extends Model<User> {
+  id: number;
+  name: string;
+  email: string;
+}
+
+declare class GuardPermission extends Model<GuardPermission> {
+  id: number;
+  name: string;
+  description: string;
+  resource: string;
+  action: string;
+  allow: number;
+}
+
+declare class GuardRole extends Model<GuardRole> {
+  id: number;
+  name: string;
+  description: string;
+  parent_id: number;
+
+  Permissions?: GuardPermission[];
+  RoleUser?: RoleUser;
+}
+
+declare class RoleUser extends Model<RoleUser> {
+  id: number;
+  role_id: number;
+  user_id: any;
+}
+
+declare class SequelizeGuard<TModel extends ModelType = ModelType> {
+  constructor(seql: Sequelize, options: SequelizeGuardOptions<TModel>);
+
+  models(): {[name: string]: Model};
+  init(): GuardControl;
+  allow(role: string, actions: Action[] | Action, resources: string[] | string): Promise<{
+    role: GuardRole,
+    permissions: GuardPermission[]
+  }>;
+  userIsA(user: TModel, role: string): Promise<boolean>;
+  createPerms(resources: string | string[], actions: Action | Action[], options?: {
+    names?: string[],
+    all?: boolean,
+    json?: false
+  }): Promise<GuardPermission[]>;
+  createPerms(resources: string | string[], actions: Action | Action[], options?: {
+    names?: string[],
+    all?: boolean,
+    json?: true
+  }): Promise<string[]>;
+  createPerms(resources: string | string[], actions: Action | Action[], options?: {
+    names?: string[],
+    all?: boolean,
+    json?: boolean
+  }): Promise<GuardPermission[]> | Promise<string[]>;
+  createPermsBulk(permissions: GuardPermission[], options: {
+    all?: boolean,
+    json?: false
+  }): Promise<GuardPermission[]>;
+  createPermsBulk(permissions: GuardPermission[], options: {
+    all?: boolean,
+    json?: true
+  }): Promise<string[]>;
+  createPermsBulk(permissions: GuardPermission[], options: {
+    all?: boolean,
+    json?: boolean
+  }): Promise<GuardPermission[]> | Promise<string[]>;
+  findPerms(args: Partial<GuardPermission & {search: boolean}>): Promise<GuardPermission>;
+  makeRole(role: string): Promise<{role: GuardRole, created: boolean}>;
+  makeRoles(roles: string | string[]): Promise<GuardRole[]>;
+  deleteRoles(roles: string | string[]): Promise<number>;
+  allRoles(): Promise<GuardRole[]>;
+  getRole(role: string): Promise<GuardRole>;
+  findRoles(args: {
+    name?: string,
+    names?: string[],
+    search?: boolean
+  }): Promise<GuardRole[]>;
+  addPermsToRole(role: string, actions: Action | Action[], resources: string | string[]): Promise<{
+    role: GuardRole,
+    permissions: GuardPermission[]
+  }>;
+  rmPermsFromRole(role: string, actions: Action | Action[], resources: string | string[]): Promise<{
+    role: GuardRole,
+    permissions: GuardPermission[],
+    permsRemoved: GuardPermission[]
+  }>
+  assignRole(user: TModel, role: string): Promise<TModel>;
+  assignRoles(user: TModel, roles: string[]): Promise<RoleUser>;
+  rmAssignedRoles(user: TModel, roles: string | string[]): Promise<void>;
+  onRolesCreated(cb: (...args: any[]) => void): () => EventEmitter;
+  onRolesDeleted(cb: (...args: any[]) => void): () => EventEmitter;
+  onPermsCreated(cb: (...args: any[]) => void): () => EventEmitter;
+  onPermsAddedToRole(cb: (...args: any[]) => void): () => EventEmitter;
+  onPermsRemovedFromRole(cb: (...args: any[]) => void): () => EventEmitter;
+  makeUser(user: Partial<TModel>): Promise<TModel>;
+  getUserRoles(user: TModel): Promise<GuardRole>;
+
+  resetCache(): NodeCache;
+  getCache(): Promise<NodeCache>;
+  resetUserCache(): NodeCache;
+  getUserCache(): NodeCache;
+
+  userHasRoles(user: T, roles: string): Promise<boolean>;
+  userHasAllRoles(user: T, roles: string): Promise<boolean>;
+}
+
+declare class GuardControl {
+  constructor(guard: SequelizeGuard);
+
+  allow(roles: string): this;
+  to(actions: Action[] | Action): this;
+  on(resources: string[] | string): this;
+  commit(): Promise<{
+    role: GuardRole,
+    permissions: GuardPermission[]
+  }>;
 }
 
 export default SequelizeGuard;
 
-export {
-  SequelizeGuardOptions
+export type {
+  SequelizeGuardOptions,
+  GuardUser,
 }
